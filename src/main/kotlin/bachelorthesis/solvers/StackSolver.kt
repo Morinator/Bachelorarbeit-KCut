8184095fe0965a17413c76cb838dcde669b896f5
Moritz Groß <moritz.gross@flipapp.de>
2022-06-06 00:12:57 +0200

    extracted method runTree(), but in a weird format sadly

--- src/main/kotlin/bachelorthesis/solvers/StackSolver.kt
+++ src/main/kotlin/bachelorthesis/solvers/StackSolver.kt
@@ -22,77 +22,82 @@ class StackSolver(
     fun calc(): Solution<Int> {
         tIncreaseLoop@ while (t <= g.degreeSequence.takeLast(k).sum()) {
 
-            var valueOfT = 0
-            val T = ArrayList<Int>()
-
-            fun cont(v: Int) = (g.degreeOf(v) + counter[v]) - (2 * intersectionSize(T, g[v]))
-
-            val sat = ArrayList<Boolean>()
-            val ext = mutableListOf(g.V.sortedDesc { cont(it) })
-
-            var tmpSolution: Solution<Int>? = null // is null <=> no fitting subset found yet
-
-            fun kMissing(): Int = k - T.size
-            fun tMissing(): Int = t - valueOfT
-            fun checkIfSatisfactory(v: Int): Boolean = (cont(v) >= (tMissing() / kMissing()) + 2 * (k - 1))
-            fun currentTreeNodeHasSatRule(): Boolean = (sat.size == T.size + 1)
-
-            searchTree@ while (ext.isNotEmpty()) {
-
-                val doSatRule = (currentTreeNodeHasSatRule() && sat.last())
-                if (doSatRule) AlgoStats.satRuleCounter++
-
-                val doBacktrack = T.size >= k || T.size + ext.last().size < k || doSatRule
-
-                if (doBacktrack) {
-
-                    if (T.size == k) {
-                        AlgoStats.candidateCounter++
-
-                        if (valueOfT >= t) {
-                            tmpSolution = Solution(T, valueOfT)
-                            t = valueOfT + 1
-                            break@searchTree
-                        }
-                    }
-
-                    if (T.size < k)
-                        ext.removeLast()
-                    if (currentTreeNodeHasSatRule())
-                        sat.removeLast()
-
-                    if (T.size > 0)
-                        valueOfT -= cont(T.removeLast())
-
-                } else { // ##### BRANCH #####
-
-                    AlgoStats.treeNodeCounter++
-
-                    val newElem = ext.last().removeFirst()
-
-                    if (T.size < k - 1) // you're not adding a leaf to the search tree  (just for faster runtime)
-                        ext.add(ext.last().sortedDesc { cont(it) })
-
-                    val isSatisfactory = checkIfSatisfactory(newElem) // TODO dont calc it useless
-                    valueOfT += cont(newElem)
-
-                    T.add(newElem)
-
-                    if (!currentTreeNodeHasSatRule()) {
-                        sat.add(isSatisfactory)
-                        AlgoStats.satVerticesCounter++
-                    }
-                }
-            }
-
-            if (tmpSolution == null)
-                break@tIncreaseLoop
-            else
-                bestSolution = tmpSolution
+            if (runTree()) break@tIncreaseLoop
         }
 
         AlgoStats.print()
 
         return bestSolution
+    }
+
+    private fun runTree(): Boolean {
+        var valueOfT = 0
+        val T = ArrayList<Int>()
+
+        fun cont(v: Int) = (g.degreeOf(v) + counter[v]) - (2 * intersectionSize(T, g[v]))
+
+        val sat = ArrayList<Boolean>()
+        val ext = mutableListOf(g.V.sortedDesc { cont(it) })
+
+        var tmpSolution: Solution<Int>? = null // is null <=> no fitting subset found yet
+
+        fun kMissing(): Int = k - T.size
+        fun tMissing(): Int = t - valueOfT
+        fun checkIfSatisfactory(v: Int): Boolean = (cont(v) >= (tMissing() / kMissing()) + 2 * (k - 1))
+        fun currentTreeNodeHasSatRule(): Boolean = (sat.size == T.size + 1)
+
+        searchTree@ while (ext.isNotEmpty()) {
+
+            val doSatRule = (currentTreeNodeHasSatRule() && sat.last())
+            if (doSatRule) AlgoStats.satRuleCounter++
+
+            val doBacktrack = T.size >= k || T.size + ext.last().size < k || doSatRule
+
+            if (doBacktrack) {
+
+                if (T.size == k) {
+                    AlgoStats.candidateCounter++
+
+                    if (valueOfT >= t) {
+                        tmpSolution = Solution(T, valueOfT)
+                        t = valueOfT + 1
+                        break@searchTree
+                    }
+                }
+
+                if (T.size < k)
+                    ext.removeLast()
+                if (currentTreeNodeHasSatRule())
+                    sat.removeLast()
+
+                if (T.size > 0)
+                    valueOfT -= cont(T.removeLast())
+
+            } else { // ##### BRANCH #####
+
+                AlgoStats.treeNodeCounter++
+
+                val newElem = ext.last().removeFirst()
+
+                if (T.size < k - 1) // you're not adding a leaf to the search tree  (just for faster runtime)
+                    ext.add(ext.last().sortedDesc { cont(it) })
+
+                val isSatisfactory = checkIfSatisfactory(newElem) // TODO dont calc it useless
+                valueOfT += cont(newElem)
+
+                T.add(newElem)
+
+                if (!currentTreeNodeHasSatRule()) {
+                    sat.add(isSatisfactory)
+                    AlgoStats.satVerticesCounter++
+                }
+            }
+        }
+
+        if (tmpSolution == null)
+            return true
+        else
+            bestSolution = tmpSolution
+        return false
     }
 }
