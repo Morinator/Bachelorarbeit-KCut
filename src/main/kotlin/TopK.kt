70d297b10387ec7d37e44bbf3fdb08b0b954d027
moritzgross <mgross321@gmail.com>
2022-08-04 19:53:57 +0200

    topK only with heap

--- src/main/kotlin/TopK.kt
+++ src/main/kotlin/TopK.kt
@@ -1,28 +1,15 @@
 import java.util.*
 
-fun <E : Comparable<E>> topKWithPivot(col: Collection<E>, k: Int): Set<E> {
-    if (k > col.size) throw IllegalArgumentException("k too big for")
-    if (k == 0) return emptySet()
-    if (col.size == k) return col.toSet()
-
-    val pivot = col.random()
-    val (geq, lower) = col.partition { it >= pivot }
-
-    return if (k <= geq.size)
-        topKWithPivot(geq, k)
-    else
-        geq.plus(topKWithPivot(lower, k - geq.size)).toSet()
-}
-
 
 fun topKWithHeap(col: Collection<Int>, k: Int): Set<Int> {
-    if (k > col.size || k < 0) throw IllegalArgumentException("Illegal value for k")
+    if (k < 0) throw IllegalArgumentException("Illegal value for k")
     if (k == 0) return emptySet()
+    if (k >= col.size) return col.toSet()
 
     val heap = PriorityQueue { a: Int, b: Int -> b - a }
     heap.addAll(col)
 
-    val res = HashSet<Int>()
-    repeat(k) { res.add(heap.remove()) }
-    return res
+    return HashSet<Int>().apply {
+        repeat(k) { add(heap.remove()) }
+    }
 }
--- src/test/kotlin/TopKTest.kt
+++ src/test/kotlin/TopKTest.kt
@@ -1,39 +1,32 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 internal class TopKTest {
 
     @Test
     fun takeAll() {
         val l = (1..10).toSet()
-        assertEquals(l, topKWithPivot(l, 10))
+        assertEquals(l, topKWithHeap(l, 10))
     }
 
     @Test
     fun kTooBig() {
-        val l = (1..10).toList()
-        assertThrows<IllegalArgumentException> { topKWithPivot(l, 11) }
+        val l = (1..10).toSet()
+        assertEquals(l, topKWithHeap(l, 10))
     }
 
     @Test
     fun takeKFrom1to100() {
-        val oneToHundred = (1..100).toList()
+        val oneToHundred = (1..100).toSet()
         for (k in 1..100) {
             val res = (100-k+1 .. 100).toSet()
-            assertEquals(res, topKWithPivot(oneToHundred, k))
+            assertEquals(res, topKWithHeap(oneToHundred, k))
         }
-    }
-
-    @Test
-    fun abcde() {
-        val abcde = listOf('a', 'b', 'c', 'd', 'e')
-        assertEquals(setOf('c', 'd', 'e'), topKWithPivot(abcde, 3))
     }
 
     @Test
     fun kIs0() {
         val l = (1..10).toSet()
-        assertEquals(emptySet<Int>(), topKWithPivot(l, 0))
+        assertEquals(emptySet<Int>(), topKWithHeap(l, 0))
     }
 }
